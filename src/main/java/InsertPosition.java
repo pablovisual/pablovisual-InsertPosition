ed4975aa6a43a1009c5cfc6f0f235223b1f027c8
Pablo Sandoval <pablo.a.sandoval01@utrgv.edu>
2023-03-05 21:51:21 -0600

    coding challenge complete

--- src/main/java/InsertPosition.java
+++ src/main/java/InsertPosition.java
@@ -15,6 +15,42 @@ public class InsertPosition {
      * @return return the index that a number should be inserted into a sorted array.
      */
     public int searchInsert(int[] nums, int target) {
-        return -1;
+        //create at index at position 0
+        int index = 0;
+
+        //return index if nums is empty
+        if(nums.length == 0)
+            return 0;
+
+        //check if nums only has one item in the array
+        else if(nums.length == 1) {
+            if(target < nums[0])
+                return 0;
+
+            return 1;
+        }
+
+        //another if there are two items in nums array
+        else if(nums.length == 2) {
+            if(target < nums[0])
+                return 0;
+
+            else if(target > nums[1])
+                return 2;
+
+            else
+                return 1;
+        }
+
+        //then lastly num array is bigger than size 2
+        for(int idx = 0; idx < nums.length; ++idx) {
+            if(target < nums[idx])
+                return idx;
+        }
+
+        //return index+=nums.length since target was bigger than the other
+        //numbers in the nums array
+        index += nums.length;
+        return index;
     }
 }
